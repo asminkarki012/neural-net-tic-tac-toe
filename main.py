b4088783fa770904f1030fd0b267528f227cb848
dR3am5t0rM <sizhe@hotmail.com>
2017-04-06 08:57:34 +0100

    Problem with weight update on backprop step.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from tictactoe import *
 from neuralnet import *
 running = True
@@ -5,16 +7,18 @@ running = True
 
 net = Neural_Net([9,9,9])
 
-training_data = [([1,0,0,0,0,0,0,0,0], [0,0,0,0,1,0,0,0,0])]
+training_data = [([1,0,0,0,0,0,0,0,0], [1,1,0,0,0,0,0,0,0])]
 
 print("Before training: ")
 
 output = net.feed_forward([1,0,0,0,0,0,0,0,0])
 print(output)
 
-for i in range(1000):
-    net.backpropagation(training_data, 0.3)
+#for i in range(10):
+net.backpropagation(training_data, 0.3)
 
-output = net.feed_forward([0 for x in range(9)])
+
 print("After training: ")
+output = net.feed_forward([1,0,0,0,0,0,0,0,0])
+print(net.weights)
 print(output)
--- neuralnet.py
+++ neuralnet.py
@@ -1,39 +1,36 @@
 import numpy as np
+import json
 
 class Neural_Net(object):
 
     def __init__(self, layers):
-
         self.layers = layers
 
         # Set random weights initially
         self.biases = [np.random.randn(x, 1) for x in layers[1:]]
-        self.weights = [0 for x, y in zip(layers[1:], layers[:-1])]
+        self.weights = [np.zeros(shape=(x, y)) for x, y in zip(layers[1:], layers[:-1])]
 
         # Activation functions
-        self.activation = sigmoid
+        self.activation = sigmoid 
         self.activation_derivative = sigmoid_derivative
 
         # Cost functions
         self.cost_derivative = mse_derivative
 
-
         #debug
         print("Input weights:")
-        print(self.weights[0])
+        print(self.weights)
 
 
     def feed_forward(self, X):
         y_hat = X
 
         for bias, weight in zip(self.biases, self.weights):
-            y_hat = sigmoid(np.dot(weight, y_hat))
+            z = np.dot(weight, y_hat)
+            y_hat = self.activation(z)
 
         return y_hat
       
-    #def gradient_descent(self, training_data, learning_rate):
-        #for (x, y) in training_data:
-
     def backpropagation(self, training_data, learning_rate):
         
         for (x, y) in training_data:
@@ -44,30 +41,53 @@ class Neural_Net(object):
             # Feed forward to get weighted input vector and output vector
             input = x
             for bias, weight in zip(self.biases, self.weights):
-                z = np.dot(weight, input)
+                z = np.dot(weight, input) 
                 zs.append(z)
-                
-                input = self.activation(np.dot(weight, input))
+
+                input = self.activation(z)
+
                 acts.append(input)
-            
+                
+                
+        
             # Error in output layer
             delta_L = np.multiply(self.cost_derivative(acts[-1], y), self.activation_derivative(zs[-1]))
-
+            
             # Backprop to get error in previous layers
             for l in range(len(self.layers)-3, -1, -1):
                 w_L = np.transpose(self.weights[l+1])
-                prev_layer = w_L * delta_L
-
-                delta_l = np.multiply(prev_layer, self.activation_derivative(zs[l]))
+                next_layer = w_L * delta_L
+                
+                delta_l = np.multiply(next_layer, self.activation_derivative(zs[l]))
                 ds.append(delta_l)
 
             ds.append(delta_L)
 
             # Updates weights
             self.weights = [w - learning_rate/len(training_data) * d * a for w, d, a in zip(self.weights, ds, acts)]
+            self.biases = [b - learning_rate/len(training_data) * d for b, d in zip(self.biases, ds)]
 
             #debug
-           
+
+    def import_network(self, json_network):
+        data = json.loads(json_network)
+        layers = data["nr_inputs"]
+
+        layers.extend([nodes for nodes in data["layers"]])
+
+
+
+    def export_network(self):
+        network = {}
+        network["nr_inputs"] = self.layers[0]
+        network["nr_ouputs"] = self.layers[-1]
+        network["nr_layers"] = len(self.layers)
+
+        layers = [] 
+
+        return json.dumps(network)
+        
+
 def sigmoid(x):
     return 1/(1+np.exp(-x)) 
 
