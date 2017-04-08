8e19b6bd736fe88d0667c6e9a10e8b6897ae1523
dR3am5t0rM <sizhe@hotmail.com>
2017-04-08 11:39:31 +0100

    Cleaned up code/comments and changed self.layers back to old version.

--- main.py
+++ main.py
@@ -6,22 +6,17 @@ from neuralnet import *
 running = True
 #game = Tictactoe()
 
-# with 3 layers, with input dimension = 9 and hidden = 4.
-net = Neural_Net([[4, 9],[5,4],[9, 5]]) 
+net = Neural_Net([9, 3, 9]) 
 input = [0.137289, 0.172, 0.75, 0.3, 0.999, 0.4123, 0.77773, 0.324, 0.6543]
 training_data = [(input, [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])]
 
-
-output_before = net.feed_forward(input)
-
-for i in range(10000):
-    net.forward_backward_prop(training_data, 0.5)
-
-
 print("Before training: ")
+output_before = net.feed_forward(input)
 print(output_before)
+
+# Training
+net.train(training_data, 0.2, 1000)
 
 print("After training: ")
 output = net.feed_forward(input)
-# print(net.weights)
 print(output)
--- neuralnet.py
+++ neuralnet.py
@@ -3,14 +3,13 @@ import json
 
 class Neural_Net(object):
     
-    # init layers with each layers' dimension, i.e. len(layers) = num_layers, layers[i] = [shape of weights]
     def __init__(self, layers):
         self.layers = layers
    
         # Set random weights initially
-        self.biases = [np.zeros([x, 1]) for x, _ in layers] 
-        self.weights = [np.random.randn(x, y) for x, y in layers]
-        
+        self.biases = [np.zeros([x, 1]) for x in layers[1:]]
+        self.weights = [np.random.randn(x, y) for x, y in zip(layers[1:], layers[:-1])]
+
         # Activation functions
         self.activation = sigmoid
         self.activation_derivative = sigmoid_derivative
@@ -24,89 +23,86 @@ class Neural_Net(object):
 
 
     def feed_forward(self, X):
-        y_hat = np.reshape(X, [len(X),-1]) # <-- very important!
+        y_hat = np.reshape(X, [len(X),-1])
         
         for bias, weight in zip(self.biases, self.weights):
-            z = np.dot(weight, y_hat)
-            print("z shape =", z.shape)
-            y_hat = self.activation(z + bias)
-            print("yhat shape =", y_hat.shape)
-        return y_hat
+            z = np.dot(weight, y_hat) + bias
+            y_hat = self.activation(z)
+
+            # Debug
+            #print("z shape = " + z.shape)
+            #print("yhat shape = " = y_hat.shape)
+
+        # Tranpose on return for readability
+        return y_hat.T
+
     
-    # changed name to forward_backward_prop for better notation
-    def forward_backward_prop(self, training_data, learning_rate):
-        
-        for (x, y) in training_data:
-            zs = [] # Weighted input vector of every layer 
-            acts = [] # Output vector of every layer
-            ds = [] # Error vector of every layer
+    def forward_backward_prop(self, x, y, learning_rate):
+        zs = [] # Weighted input vector of every layer 
+        acts = [] # Output vector of every layer
+        ds = [] # Error vector of every layer
 
-            # Feed forward to get weighted input vector and output vector
-            # bias shape is [x,1], but z has shape [x,] !
-            input = np.reshape(x, [len(x),-1])
-            y = np.reshape(y, [len(y),-1])
-            # ^ this is important!!!!!!! make sure input shape is in form [x,1], not [x,]
+        # Reshape x and  y to be of the form [n, 1]
+        input = np.reshape(x, [len(x),-1])
+        y = np.reshape(y, [len(y),-1])
+        #print("input shape =", input.shape)
 
-            #print("input shape =", input.shape)
-            for bias, weight in zip(self.biases, self.weights):
-                acts.append(input)
-           
-                z = np.dot(weight, input) + bias
-                zs.append(z)
-                #print("z shape =", z.shape)
-                #print("bias shape =", bias.shape)
-                input = self.activation(z)
-                #print("input shape = ", input.shape)
-                
-            #print("acts.shape =", [i.shape for i in acts])
-            # Error in output layer
-            #print("cost derivative shape =", self.cost_derivative(input, y).shape)
-            # wrong shape! cost_derivative should give 9*1 shape, not 9*9! The error occurred because label has shape [x,]!
-            delta_L = np.multiply(self.cost_derivative(input, y), self.activation_derivative(zs[-1]))
-            
-            # should append this delta_L first
-            ds.append(delta_L)
-            
-            #print("error: " + str(np.linalg.norm(self.cost(input, y))))
-            #print("errrs: " + str(np.linalg.norm(delta_L)))
-            
-            # Backprop to get error in previous layers
-            for l in range(len(self.layers)-2, -1, -1):   #<- changed to -2 to fit new self.layers format
-                w_L = np.transpose(self.weights[l+1]) 
-                # you did not loop the delta_L here! you used delta_l below!
-                next_layer = np.dot(w_L, delta_L) # <- also use np.dot here! they are matrices!
+        # Feed forward to get weighted input vector and output vector
+        for bias, weight in zip(self.biases, self.weights):
+            acts.append(input)
 
-                #print("next layer" + str(next_layer))
-                
-                delta_L = np.multiply(next_layer, self.activation_derivative(zs[l]))
+            z = np.dot(weight, input) + bias
+            zs.append(z)
+            input = self.activation(z)
 
-                #print("dleta_l" + str(delta_l))
-                
-                # for ds, you appended it backwards! so ds[0] is actually the final layer! hence you got to append it in front.
-                #ds.append(delta_L)
-                ds = [delta_L] + ds
+            # Debug
+            #print("z shape =", z.shape)
+            #print("bias shape =", bias.shape)
+            #print("input shape = ", input.shape)
 
-            #ds.append(delta_L) #<- why append here?
-            #print("ds.shape =", [i.shape for i in ds])
-            
-            # Updates weights
-            #for w, d, a in zip(self.weights, ds, acts):
-            #    print("weight: " + str(w))
-            #    print("deltas: " + str(d))
-            #    print("actives " + str(a))
-            #    print("d * a : " + str(w - np.multiply(d, np.matrix(a).T)))
-            
-            # these are matrices! you got to use dot product!!!! still not sure why len(train_data) is used here,
-            # remember to transpose a! This part is tricky
-            self.weights = [w - learning_rate/len(training_data) * np.dot(d, np.array(a).T) for w, d, a in zip(self.weights, ds, acts)]
-            #print("weight after update shape =", [w.shape for w in self.weights])
-            
-            self.biases = [b - learning_rate/len(training_data) * d for b, d in zip(self.biases, ds)]
-            #print("bias after update shape =", [b.shape for b in self.biases])
-            
+        #print("acts.shape =", [i.shape for i in acts])
+        #print("cost derivative shape =", self.cost_derivative(input, y).shape)
 
-            #debug
+        # Error in output layer
+        delta_L = np.multiply(self.cost_derivative(input, y), self.activation_derivative(zs[-1]))
 
+        ds.append(delta_L)
+
+        #print("error: " + str(np.linalg.norm(self.cost(input, y))))
+        #print("errrs: " + str(np.linalg.norm(delta_L)))
+
+        # Backprop to get error in previous layers
+        for l in range(len(self.layers)-3, -1, -1):
+            w_L = np.transpose(self.weights[l+1]) 
+            next_layer = np.dot(w_L, delta_L)
+
+            delta_L = np.multiply(next_layer, self.activation_derivative(zs[l]))
+
+            # Prepend error of the layer
+            ds = [delta_L] + ds
+
+        #print("ds.shape =", [i.shape for i in ds])
+
+        # Updates weights and biases
+        self.weights = [w - learning_rate * np.dot(d, np.array(a).T) for w, d, a in zip(self.weights, ds, acts)]
+
+        self.biases = [b - learning_rate * d for b, d in zip(self.biases, ds)]
+
+        # Debug
+        #print("weight after update shape =", [w.shape for w in self.weights])
+        #print("bias after update shape =", [b.shape for b in self.biases])
+
+
+    def train(self, training_data, learning_rate, epochs):
+        for i in range(epochs):
+            for (x, y) in training_data:
+                self.forward_backward_prop(x, y, learning_rate)
+
+            if i % 100 == 0:
+                output = self.feed_forward(x)
+                print("Epoch " + str(i) + " - Error: " + str(np.linalg.norm(self.cost(output, y))))
+
+            
     def import_network(self, json_network):
         data = json.loads(json_network)
         layers = data["nr_inputs"]
